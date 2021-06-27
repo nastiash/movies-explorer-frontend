7282c8ba206b6b971ba63f88f44cb902c4ac5d55
Nastia Shuyskaya <anst.shy@yandex.ru>
2021-06-27 12:22:48 +0300

    созданы файлы mainApi и moviesApi

---
+++ src/utils/moviesApi.js
@@ -0,0 +1,96 @@
+export default class Api {
+  constructor(config) {
+    this.url = config.url;
+    this.headers = config.headers;
+  }
+
+  _getResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(new Error(`Ошибка: ${res.status}`));
+  }
+
+  getUserInfo(token) {
+    return fetch(`${this.url}/users/me`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => this._getResponse(res));
+  }
+
+  updateProfile({ name, email }) {
+    const token = localStorage.getItem("token");
+    return fetch(`${this.url}/users/me`, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ name, email }),
+    }).then((res) => this._getResponse(res));
+  }
+
+  getSavedMovies(token) {
+    return fetch(`${this.url}/movies`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => this._getResponse(res));
+  }
+
+  addMovie(movie) {
+    const token = localStorage.getItem("token");
+    return fetch(`${this.url}/movies`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        country: movie.country,
+        director: movie.director,
+        duration: movie.duration,
+        year: movie.year,
+        description: movie.description,
+        image: `https://api.nomoreparties.co${movie.image.url}`,
+        trailer: movie.trailerLink,
+        thumbnail: `https://api.nomoreparties.co${movie.image.url}`,
+        movieId: movie.id,
+        nameRU: movie.nameRU,
+        nameEN: movie.nameEN,
+      }),
+    }).then((res) => this._getResponse(res));
+  }
+
+  deleteMovie(movieId) {
+    const token = localStorage.getItem("token");
+    return fetch(`${this.url}/movies/${movieId}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => {
+      return this._getResponse(res);
+    });
+  }
+}
+
+export const mainApi = new Api({
+  url: "https://api.nastiash.diploma.nomoredomains.icu",
+  headers: {
+    Accept: "application/json",
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+    "Content-Type": "application/json",
+  },
+});
